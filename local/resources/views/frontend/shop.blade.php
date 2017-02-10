717c48aea67cbb9d8244deb3ecfb919aa96ad24a
Rodrigo Méndez <rodrigo@apto.mx>
2017-02-10 17:38:06 -0600

    fix descripcion productos

--- local/resources/views/frontend/shop.blade.php
+++ local/resources/views/frontend/shop.blade.php
@@ -69,7 +69,7 @@
                  
                   <div class="content">
                       <h3>{{$producto->nombre}}</h3></a>
-                      <span class="price">{{str_limit(strip_tags($producto->description), 150)}}...</span>
+                      <span class="price">{{str_limit(strip_tags($producto->descripcion), 150)}}...</span>
                   </div>
                 </div>
               </div>
