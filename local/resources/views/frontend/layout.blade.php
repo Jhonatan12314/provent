01f9606adb5839e4546fefae2b1bcaf947659100
Rodrigo Méndez <rodrigo@apto.mx>
2017-02-13 17:17:40 -0600

    Chat

--- local/resources/views/frontend/layout.blade.php
+++ local/resources/views/frontend/layout.blade.php
@@ -78,5 +78,19 @@
     <!--script src="js/theme.js"></script-->
     <script type='text/javascript' src="{{url('public/frontend/js/theme.js')}}"></script>
 
+    <!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/5892155b64d4e50a6eb4da9e/default';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
+
 <!-- Mirrored from demos.pixelatethemes.com/industrial/contact by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 10 Jan 2017 22:15:37 GMT -->
 </html>
