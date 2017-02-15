30243a0338ea4a316bb2a1169723ac97ab23d7ff
Rodrigo Méndez <rodrigo@apto.mx>
2017-02-15 14:15:02 -0600

    instagram

--- local/resources/views/frontend/layout.blade.php
+++ local/resources/views/frontend/layout.blade.php
@@ -184,7 +184,7 @@
       <a href="{{$configuracion->pinterest}}" class="contact-button-link cb-ancor" style="background-color: #bd081c;"><span class="fa fa-pinterest"></span></a>
       <a href="tel:{{$configuracion->skype}}" class="contact-button-link cb-ancor" style="background-color: #00AFF0;"><span class="fa fa-skype"></span></a>
       <a href="{{$configuracion->google}}" class="contact-button-link cb-ancor" style="background-color: #dd4b39;"><span class="fa fa-google-plus"></span></a>
-      <a href="tel:{{$configuracion->whatsapp}}" class="contact-button-link cb-ancor" style="background-color: #25D366;"><span class="fa fa-whatsapp"></span></a>
+      <a href="{{$configuracion->whatsapp}}" class="contact-button-link cb-ancor" style="background-color: #25D366;"><span class="fa fa-whatsapp"></span></a>
       <a href="{{$configuracion->linkedin}}" class="contact-button-link cb-ancor linkedin" ><span class="fa fa-linkedin"></span></a>
       <a href="{{$configuracion->instagram}}" class="contact-button-link cb-ancor instagram" style="background-color: #e4405f"><span class="fa fa-instagram"></span></a>
 
