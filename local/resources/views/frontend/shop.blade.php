90abee4dd054ade5f4e207605569ef8cd28e69ae
Rodrigo Méndez <rodrigo@apto.mx>
2017-02-21 17:08:58 -0600

    arrelgo busqueda de productos

--- local/resources/views/frontend/shop.blade.php
+++ local/resources/views/frontend/shop.blade.php
@@ -28,7 +28,7 @@
               <!-- Search-->
               <div class="widget-search-blog">
                 <div class="row widget-inner">
-                  <form action="" method="get" class="search-form">
+                  <form action="{{url('solutions')}}" method="get" class="search-form">
                     <div class="input-group">
                       <input type="search" placeholder="Buscar producto..." name="search" class="form-control"><span class="input-group-addon">
                         <button type="submit"><i class="icon icon-Search"></i></button></span>
